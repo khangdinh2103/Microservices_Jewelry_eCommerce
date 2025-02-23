1525a635e1c638a9034ef90178ffbd18d648c82c
Khang Dinh <21123341.khang@student.iuh.edu.vn>
2025-02-23 23:16:58 +0700

    upDateCartItem

--- Workspace/Service_Cart_Order/Microservices_Jewelry_eCommerce/src/routers/cartRoutes.js
+++ Workspace/Service_Cart_Order/Microservices_Jewelry_eCommerce/src/routers/cartRoutes.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const { getCartByUserId, addCartItem, updateCartItemQuantity } = require('../controllers/cartController');
 
 
-// API: Lấy giỏ hàng theo user ID
 router.get('/cart/:userId', getCartByUserId);
 router.post('/cart-items', addCartItem);
 router.put('/cart-items/:cartItemID', updateCartItemQuantity);
 
 
-//note
 module.exports = router;
